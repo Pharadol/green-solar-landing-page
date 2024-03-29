85a180a1995251ff0f74db5dd29e24d2000dccc8
Pharadol <pharadol.n@gmail.com>
2024-03-29 21:10:05 +0700

    feat: add icon

---
+++ src/assets/icon/Hamburger.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+export default function Hamburger({ className }) {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className={className || "w-6 h-6"}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+        />
+      </svg>
+    </>
+  );
+}
+Hamburger.propTypes = {
+  className: PropTypes.string,
+};
---
+++ src/assets/icon/Xmark.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+
+export default function Xmark({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className || "w-6 h-6"}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  );
+}
+
+Xmark.propTypes = {
+  className: PropTypes.string,
+};
